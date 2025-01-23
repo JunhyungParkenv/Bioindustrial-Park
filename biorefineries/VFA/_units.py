46a4a23315eff00d93546fa363fe7ddf5b597d5a
JunhyungParkenv <145532861+JunhyungParkenv@users.noreply.github.com>
2025-01-22 22:48:36 -0600

    Update _units.py

--- biorefineries/VFA/_units.py
+++ biorefineries/VFA/_units.py
@@ -222,7 +222,7 @@ class ED(bst.Unit):
     _N_outs = 2  # eff_dc, eff_ac
 
     def __init__(self, ID='', ins=None, outs=(), thermo=None, CE_dict=None, j=5.058, 
-                 A_m=None, R=0.0000222, z_T=1.0, t=24*3600, dc_tau=24, target_ratio=0.8):
+                 A_m=None, R=0.0000222, z_T=1.0, t=24*3600, target_ratio=0.8):
         super().__init__(ID, ins, outs, thermo=thermo)
         self.CE_dict = CE_dict or {
             'AceticAcid': 0.164472, 'PropionicAcid': 0.082236, 'ButyricAcid': 0.059,
@@ -234,12 +234,6 @@ class ED(bst.Unit):
         self.z_T = z_T
         self.t = t
         self.target_ratio = target_ratio
-        self.dc_tau = dc_tau
-        self.ac_tau = dc_tau / 4
-        
-        # DC 및 AC 탱크 연결
-        self.dc_storage = DC_Tank(f"{ID}_dc_tank", tau=self.dc_tau)
-        self.ac_storage = AC_Tank(f"{ID}_ac_tank", tau=self.ac_tau)
 
     def calculate_flux(self, I):
         J_T_dict = {ion: (CE * I) / (self.z_T * F * self.A_m) for ion, CE in self.CE_dict.items()}
@@ -272,19 +266,6 @@ class ED(bst.Unit):
 
         eff_dc.imol['Water'] = inf_dc.imol['Water']
         eff_ac.imol['Water'] = inf_ac.imol['Water']
-        
-        # DC 및 AC 탱크 연결 및 시뮬레이션
-        self.dc_storage.ins[:] = [eff_dc]
-        self.dc_storage.outs[:] = [eff_dc]
-        self.dc_storage.simulate()
-        
-        self.ac_storage.ins[:] = [eff_ac]
-        self.ac_storage.outs[:] = [eff_ac]
-        self.ac_storage.simulate()
-        
-        # Update outs explicitly
-        self.outs[0] = self.dc_storage.outs[0]
-        self.outs[1] = self.ac_storage.outs[0]
         
     _units = {
         'Membrane area': 'm^2',
